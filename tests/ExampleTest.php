705e55068e6a0dc9e16f8e95e2bd32991473c6bc
Administrator <admin@example.com>
2016-04-20 14:51:16 -0500

    pruebas en el dojo

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,4 +15,28 @@ class ExampleTest extends TestCase
         $user = App\User::find(1);
         $this->actingAs($user)->visit('/admin')->see('aaa');
     }
+
+    public function testPruebaLogueo(){
+    	$this->visit('/')
+         ->type('Jose.tbjc@gmail.com', 'email')
+         ->type('123456', 'password')
+         ->press('Submit')
+         ->seePageIs('/admin');
+    }
+
+    public function testPruebaLogueoFallido(){
+    	$this->visit('/')
+         ->type('qweqweqw', 'email')
+         ->type('qqq', 'password')
+         ->press('Submit')
+         ->seePageIs('/');
+    }
+    public function testPruebaLogueoFalso(){
+    	$user = factory(App\User::class)->make();
+    	$this->visit('/')
+         ->type($user->email, 'email')
+         ->type($user->password, 'password')
+         ->press('Submit')
+         ->seePageIs('/admin');
+    }
 }
